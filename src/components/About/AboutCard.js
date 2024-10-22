2f68588bfee120b2a2be0062cfc1111e2e0e807d
Meliora <lilian.deon29@.com>
2024-10-22 13:58:57 +0300

    changed the relative path of my cv to the correct one

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,22 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Dion Okongo </span>
+            from <span className="purple"> Nairobi, Kenya.</span>
             <br />
             I am currently employed as a software developer at Juspay.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed undergraduate Bachelors degree in Computer Science.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Listening To Music
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
@@ -34,7 +36,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Dionokongo</footer>
         </blockquote>
       </Card.Body>
     </Card>
